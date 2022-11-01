22c6ef1cb3c8ff1ffb6cd5e0c1cb4f5a918d4837
uriassmira@gmail.com <uriassmira@gmail.com>
2022-10-31 23:32:36 -0700

    Completed W07 Chamber Discover Assignment

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -35,3 +35,55 @@ if ((day == 1) || (day == 2)) {
 else {
     join.textContent = ''
 }
+
+//Lazy Loading
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+  //User Visits
+
+  // initialize display elements
+// const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+// todayDisplay.textContent = Date.now();
