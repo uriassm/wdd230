ba6caf900b4374b0b1691e9e5fdbcdf09938b65d
uriassmira@gmail.com <uriassmira@gmail.com>
2022-11-06 23:33:45 -0800

    Added days since last visit in LS for discover pg.

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -65,25 +65,43 @@ if ("IntersectionObserver" in window) {
     });
   }
 
-  //User Visits
+//******User Visits*******
 
-  // initialize display elements
+// initialize display elements
 // const todayDisplay = document.querySelector(".today");
-const visitsDisplay = document.querySelector(".visits");
+// const visitsDisplay = document.querySelector(".visit");
+const daysDisplay = document.querySelector(".daysSince");
 
-// get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastVisit = Number(window.localStorage.getItem("visitDays-ls"));
 
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+currentTime = Date.now();
+daysPassedMS = currentTime - lastVisit;
+daysPassedDays = Math.floor(daysPassedMS / 86400000)
+
+
+if (lastVisit != 0) {
+  daysDisplay.textContent = daysPassedDays;
 } else {
-	visitsDisplay.textContent = `This is your first visit!`;
+  daysDisplay.textContent = "This is your first visit!";
 }
 
+localStorage.setItem("visitDays-ls", currentTime)
+
+
+
+// get the stored value in localStorage
+// let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+// if (numVisits !== 0) {
+// 	visitsDisplay.textContent = numVisits;
+// } else {
+// 	visitsDisplay.textContent = `This is your first visit!`;
+// }
+
 // increment the number of visits.
-numVisits++;
+// numVisits++;
 // store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
+// localStorage.setItem("visits-ls", numVisits);
 // show todays date.
 // todayDisplay.textContent = Date.now();
