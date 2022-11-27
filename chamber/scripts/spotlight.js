333d80de716e27b9cc218e8d791d06b291e5b464
uriassmira@gmail.com <uriassmira@gmail.com>
2022-11-26 21:22:01 -0800

    Completed Chamber Site W11.

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -16,6 +16,9 @@ fetch(jsonData)
     secondBusinessIndex = Math.floor(Math.random() * spotlightBusinesses.length);
     secondBusiness = spotlightBusinesses[secondBusinessIndex]
     spotlightBusinesses.splice(secondBusinessIndex, 1)
+    thirdBusinessIndex = Math.floor(Math.random() * spotlightBusinesses.length);
+    thirdBusiness = spotlightBusinesses[thirdBusinessIndex]
+    spotlightBusinesses.splice(secondBusinessIndex, 1)
 
     spotlightHeading1 = document.querySelector('#spotlight1 h3');
     spotlightImage1 = document.querySelector('#spotlight1 img');
@@ -32,6 +35,14 @@ fetch(jsonData)
     spotlightImage2.setAttribute('src', secondBusiness.image);
     spotlightImage2.setAttribute('alt', `Logo of ${secondBusiness.name}`);
     spotlightContact2.textContent = `${secondBusiness.phonenumber} ${secondBusiness.website}`
+
+    spotlightHeading3 = document.querySelector('#spotlight3 h3');
+    spotlightImage3 = document.querySelector('#spotlight3 img');
+    spotlightContact3 = document.querySelector('#spotlight3 p');
+    spotlightHeading3.textContent = thirdBusiness.name;
+    spotlightImage3.setAttribute('src', thirdBusiness.image);
+    spotlightImage3.setAttribute('alt', `Logo of ${thirdBusiness.name}`);
+    spotlightContact3.textContent = `${thirdBusiness.phonenumber} ${thirdBusiness.website}`
   });
 
 function addToSpotlightArray(business) {
